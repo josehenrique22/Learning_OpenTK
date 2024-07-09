0130fb002cf8c51eab6632f25add88533a1148e3
joser <joserbtgt@gmail.com>
2024-07-09 13:06:01 -0300

    update windows

---
+++ basicOpenTK/Game.cs
@@ -0,0 +1,15 @@
+using OpenTK;
+using OpenTK.Windowing.Desktop;
+
+namespace BasicOpenTK{
+    public class Game : GameWindow
+    {
+        public Game()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+
+        }
+
+        
+    }
+}
---
+++ basicOpenTK/Program.cs
@@ -0,0 +1,14 @@
+
+namespace BasicOpenTK{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Game game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}

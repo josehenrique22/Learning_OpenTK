b99d945f4097986421ee0c1a8009a12c1b56e0c9
joser <joserbtgt@gmail.com>
2024-07-09 10:42:36 -0300

    first commit

---
+++ createwindow/Program.cs
@@ -0,0 +1,14 @@
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowsSettings = new NativeWindowSettings();
+        }
+    }
+}

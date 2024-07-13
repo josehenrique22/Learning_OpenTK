71c6445289a063a13c989920504ea9b77629a676
joser <joserbtgt@gmail.com>
2024-07-13 10:30:20 -0300

    reset

--- basicOpenTK/Program.cs
+++ basicOpenTK/Program.cs
@@ -3,9 +3,10 @@ namespace BasicOpenTK{
 
     class Program
     {
+        // recebe a instancia de Game e executa os parametros definidos pelo Costrutor
         static void Main(string[] args)
         {
-            using(Game game = new Game())
+            using(Game game = new Game(800, 600, "LearnOpenTK"))
             {
                 game.Run();
             }
